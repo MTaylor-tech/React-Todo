208cef440d2f91f067a458b4b1ddcf7db471df50
Matthew Taylor <majikpig@gmail.com>
2020-04-07 21:49:08 -0400

    full functions

---
+++ src/components/Todo.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TodoDiv = styled.div`
+  margin: 2vh 2vw;
+
+  label {
+    font-size: 2.4rem;
+    margin-left: 2vw;
+  }
+
+  .strikeThru {
+    text-decoration: line-through;
+    color: grey;
+  }
+`;
+
+class Todo extends React.Component {
+  constructor (props) {
+    super();
+    this.state = {
+      taskName: props.task.task,
+      completed: props.task.completed,
+      id: props.task.id
+    };
+  }
+
+  changeHandler = (event) => {
+    const completed = !this.state.completed;
+    this.setState({completed: completed});
+    this.props.completedFunction(this.state.id, completed);
+  }
+
+  render() {
+    return (
+      <TodoDiv>
+        {this.state.completed?<input name={this.state.id} type="checkbox" defaultChecked onChange={this.changeHandler} />:<input name={this.state.id} type="checkbox" onChange={this.changeHandler} />}
+        {this.state.completed?<label htmlFor={this.state.id} className="strikeThru">{this.state.taskName}</label>:<label htmlFor={this.state.id}>{this.state.taskName}</label>}
+      </TodoDiv>
+    );
+  }
+}
+
+export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,16 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+class TodoList extends React.Component {
+  render() {
+    return(
+      <div>
+        {this.props.list.map(t=><Todo task={t} key={t.id} completedFunction={this.props.completedFunction} />)}
+      </div>
+    );
+  }
+}
+
+export default TodoList;
